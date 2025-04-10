93ac5200113a8027e0ef1eae22b02af62c367f8b
George Parks <60108810+geoge23@users.noreply.github.com>
2025-04-09 20:50:33 -0700

    Refactor variable declaration for management group ID and add virtual network resource

--- terraform/project-one/main.tf
+++ terraform/project-one/main.tf
@@ -14,10 +14,10 @@ variable "subscription" {
   type        = string
 }
 
-variable "management_group_id" {
-  description = "The management group ID to associate the subscription with"
-  type        = string
-}
+# variable "management_group_id" {
+#   description = "The management group ID to associate the subscription with"
+#   type        = string
+# }
 
 # TODO: FOR WHEN we can autoprovision the subscription
 # provider "azurerm" {
@@ -48,4 +48,12 @@ provider "azurerm" {
 resource "azurerm_resource_group" "test-rg" {
   name     = "test-rg"
   location = "West US 3"
+}
+
+# create a virtual network
+resource "azurerm_virtual_network" "test" {
+  name                = "test-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.test-rg.location
+  resource_group_name = azurerm_resource_group.test-rg.name
 }
