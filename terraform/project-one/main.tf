8ed48b6354275d467c372b22217f7bc47918b382
George Parks <60108810+geoge23@users.noreply.github.com>
2025-04-09 20:48:33 -0700

    Add Azure provider configuration and state management for project-one

--- terraform/project-one/main.tf
+++ terraform/project-one/main.tf
@@ -14,23 +14,35 @@ variable "subscription" {
   type        = string
 }
 
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription
-  alias           = "root"
+variable "management_group_id" {
+  description = "The management group ID to associate the subscription with"
+  type        = string
 }
 
-resource "azurerm_subscription" "test-subscription" {
-  alias             = "test-subscription"
-  subscription_name = "Deployment Test"
-  subscription_id   = "62204af3-1be7-4e57-bfa2-25a70219f703"
-  provider          = azurerm.root
-}
+# TODO: FOR WHEN we can autoprovision the subscription
+# provider "azurerm" {
+#   features {}
+#   subscription_id = var.subscription
+#   alias           = "root"
+# }
+
+# resource "azurerm_subscription" "test-subscription" {
+#   alias             = "test-subscription"
+#   subscription_name = "Deployment Test"
+#   subscription_id   = "62204af3-1be7-4e57-bfa2-25a70219f703"
+#   provider          = azurerm.root
+# }
+
+# resource "azurerm_management_group_subscription_association" "add_to_mg" {
+#   management_group_id = "/providers/Microsoft.Management/managementGroups/${var.management_group_id}"
+#   subscription_id     = "/subscriptions/${azurerm_subscription.test-subscription.subscription_id}"
+#   provider            = azurerm.root
+# }
 
 provider "azurerm" {
   features {}
   resource_provider_registrations = "none"
-  subscription_id                 = azurerm_subscription.test-subscription.subscription_id
+  subscription_id                 = "62204af3-1be7-4e57-bfa2-25a70219f703"
 }
 
 resource "azurerm_resource_group" "test-rg" {
