e89c65aea22f68dee91d5382a79a3a9f8bf56514
George Parks <60108810+geoge23@users.noreply.github.com>
2025-04-16 22:05:46 -0700

    Remove version specifications for providers in workload module

--- tf-modules/workload-module/providers.tf
+++ tf-modules/workload-module/providers.tf
@@ -2,18 +2,15 @@ terraform {
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = "4.25.0"
       configuration_aliases = [azurerm.shared_services, azurerm.networking]
     }
 
     random = {
-      source  = "hashicorp/random"
-      version = "3.7.1"
+      source = "hashicorp/random"
     }
 
     azuread = {
-      source  = "hashicorp/azuread"
-      version = "3.2.0"
+      source = "hashicorp/azuread"
     }
   }
 }
